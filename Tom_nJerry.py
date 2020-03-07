a1701057c366285825823c799cdf79fc9797466c
Bharath Kumar Shivakumar <bskumar1@outlook.com>
2020-03-06 19:19:58 -0500

    Cats and a Mouse
    The task was to find the absolute difference between two integers in reference to the third integer. Return the label of the lowest integer.

---
+++ Tom_nJerry.py
@@ -0,0 +1,19 @@
+def catAndMouse(x, y, z):
+    xDist = abs(z-x)
+    yDist = abs(z-y)
+    if xDist == yDist:
+        return('Mouse C')
+    elif xDist < yDist:
+        return('Cat A')
+    else:
+        return('Cat B')
+
+if __name__ == '__main__':
+    q = int(input())
+    for q_itr in range(q):
+        xyz = input().split()
+        x = int(xyz[0])
+        y = int(xyz[1])
+        z = int(xyz[2])
+        result = catAndMouse(x, y, z)
+        print(result + '\n')
