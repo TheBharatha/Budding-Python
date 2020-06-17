dc680df137555c2de7735120008dbbc71250f8c5
BharathKumarS <bskumar1@outlook.com>
2020-06-17 17:20:49 -0500

    Removed function assignments types

--- CandyKids.py
+++ CandyKids.py
@@ -1,5 +1,5 @@
 class Solution:
-    def kidsWithCandies(self, candies: List[int], extraCandies: int) -> List[bool]:
+    def kidsWithCandies(self, candies, extraCandies):
         maxCandies = max(candies)
         distribution = list(map(lambda x: ((x+extraCandies) >= maxCandies), candies))
         return(distribution)
