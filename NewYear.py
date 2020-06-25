f45cafdb992c0199d34f9280c9053fd2e7b0a76e
BharathKumarS <bskumar1@outlook.com>
2020-06-24 23:18:09 -0500

    HackerRank, No Idea

--- NewYear.py
+++ NewYear.py
@@ -1,11 +1,9 @@
-import operator
-a = [2,1,5,3,4]
-b = sorted(a)
-c = list(map(operator.sub, a, b))
-c = list(map(str,c))
-if '3' in c:
-    print('Too chaotic')
-else:
-    c = list(filter(lambda  a: a>0, list(map(int,c))))
-    c = sorted(set(c))
-    print(sum(c))
+n, m = map(int, input().split())
+array = list(map(int, input().split()))
+a = set(map(int, input().split()))
+b = set(map(int, input().split()))
+happiness = 0
+for elem in array:
+    happiness += elem in a
+    happiness -= elem in b
+print(happiness)
