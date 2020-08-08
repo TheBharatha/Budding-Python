ec52d619b0451a90c65e9d6bb5cb6fefdec3787b
Bharath Kumar Shivakumar <bskumar1@outlook.com>
2020-08-08 18:33:17 -0500

    Update Two-Sum_LeetCode.py
    Changed the approach, 24% faster and 73.20% less space consuming

--- Two-Sum_LeetCode.py
+++ Two-Sum_LeetCode.py
@@ -1,12 +1,6 @@
 class Solution:
     def twoSum(self, nums, target):
-        anums = nums
-        for i in range(len(nums)-1):
-            for j in range(i+1,len(anums)):
-                if nums[i]+anums[j] == target:
-                    return[i,j]
-                    
-if __name__ == '__main__':
-    peek = Solution()
-    send = peek.twoSum([2, 7, 11, 15], 9)
-    print(send)
+        for pos, value in enumerate(nums):
+            diff = target - nums[pos]
+            if diff in nums and pos != nums.index(diff):
+                return [pos, nums.index(diff)]
