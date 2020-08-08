d11a4f3f052fd563e05937121e67ab97ff204301
Bharath Kumar Shivakumar <bskumar1@outlook.com>
2020-08-08 18:36:37 -0500

    Update Two-Sum_LeetCode.py
    Added the border cases solution

--- Two-Sum_LeetCode.py
+++ Two-Sum_LeetCode.py
@@ -1,6 +1,9 @@
 class Solution:
     def twoSum(self, nums, target):
-        for pos, value in enumerate(nums):
-            diff = target - nums[pos]
-            if diff in nums and pos != nums.index(diff):
-                return [pos, nums.index(diff)]
+        if len(nums) == 2:
+            return [0,1]
+        else:
+            for pos, value in enumerate(nums):
+                diff = target - nums[pos]
+                if diff in nums and pos != nums.index(diff):
+                    return [pos, nums.index(diff)]
